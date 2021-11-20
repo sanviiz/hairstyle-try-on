33e63cd82e3fae1af22d1491d5783d502236190a
phalapat <palapatmc@gmail.com>
2021-11-20 11:57:14 +0700

    Edit: default parameter variable

--- image_artifact_fill/artifact_fill.py
+++ image_artifact_fill/artifact_fill.py
@@ -18,15 +18,15 @@ def convert_to_gray(image):
     return cv2.cvtColor(image.astype('uint8'), cv2.COLOR_RGB2GRAY)
 
 
-def background_artifact_fill(original_image, original_mask, new_image):
+def background_artifact_fill(original_image, original_mask, new_image, background_radius):
     original_mask_gray = cv2.cvtColor(original_mask,
                                       cv2.COLOR_RGB2GRAY)
     object_mask = np.where(original_mask_gray != 0, 255, 0)
     object_mask = object_mask.astype('uint8')
     dilation_kernel = np.ones((13, 13), np.uint8)
     dilation_mask = cv2.dilate(object_mask, dilation_kernel, iterations=1)
-    filled_background = cv2.inpaint(original_image,
-                                    dilation_mask, 25, cv2.INPAINT_TELEA)
+    filled_background = cv2.inpaint(original_image, dilation_mask,
+                                    background_radius, cv2.INPAINT_TELEA)
     filled_background = np.where(new_image == np.array([255, 255, 255]),
                                  filled_background, new_image)
 
@@ -34,12 +34,12 @@ def background_artifact_fill(original_image, original_mask, new_image):
 
 
 def face_artifact_fill(original_image, original_mask, new_image,
-                       new_mask, new_segment):
+                       new_mask, new_segment, face_radius=5, background_radius=25):
     filled_bg_artifact = background_artifact_fill(
-        original_image, original_mask, new_image).astype('uint8')
+        original_image, original_mask, new_image, background_radius).astype('uint8')
     diff = np.where((convert_to_gray(new_mask) == convert_to_gray(new_segment)),
                     0, 255).astype('uint8')
     filled_face_artifact = cv2.inpaint(filled_bg_artifact, diff,
-                                       5, cv2.INPAINT_TELEA)
+                                       face_radius, cv2.INPAINT_TELEA)
 
     return filled_face_artifact
